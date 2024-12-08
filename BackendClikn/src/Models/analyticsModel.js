f6ec160cea2d98fb50dbbcac2ca1abc4ef919ee1
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2024-12-08 17:43:56 +0530

    added models for the analytics

---
+++ BackendClikn/src/Models/analyticsModel.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const analyticsSchema = new Schema(
+  {
+    urlId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Url",
+      required: true,
+    },
+    clikedUrl: {
+      type: [
+        {
+          date: {
+            type: Date,
+            required: true,
+          },
+          deviceName: {
+            type: String,
+            required: true,
+          },
+          device: {
+            type: String,
+            required: true,
+          },
+          location: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
+export const Analytics = mongoose.model("Analytics", analyticsSchema);
---
+++ BackendClikn/src/Models/urlmodel.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const urlSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    urlId: {
+      type: String,
+      required: true,
+    },
+    originalUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Url = mongoose.model("Url", urlSchema);
