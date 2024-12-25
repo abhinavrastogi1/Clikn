cce9ad3a9d2157683ec89521494f291541c71f44
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2024-12-26 02:20:33 +0530

    added links to footer

--- UiClikn/src/Component/Footer/Footer.jsx
+++ UiClikn/src/Component/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import instagramIcon from "../../assets/instagramIcon.svg"
+import linkedinIcon from "../../assets/linkedinIcon.svg"
 function Footer() {
+  const navigate=useNavigate()
   return (
     <footer
       className="footer  text-neutral-content items-center p-4 flex justify-between
@@ -15,41 +18,40 @@ function Footer() {
             src="/cliknLogo.png"
             className="h-10 w-20 sm:h-16 sm:w-32"
             alt="clikn Logo"
+            role="button"
+            onClick={()=>{
+              navigate("/")
+            }}
           />
           <nav className="flex gap-4 md:place-self-center md:justify-self-end  ">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current dark:text-white"
+          <a
+                href="https://www.instagram.com/abh.inav_ras.togi/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 hover:underline shadow-md rounded-md dark:shadow-white bg-white
+              hover:-translate-y-3 duration-500 ease-in-out 
+              "
               >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current dark:text-white"
+                <img
+                  src={instagramIcon}
+                  alt="instagram Icon"
+                  className="h-10 w-10 "
+                />
+              </a>
+          <a
+                href="https://www.linkedin.com/in/abhinav-rastogi-a664612a3/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 hover:underline shadow-md rounded-md dark:shadow-white bg-white
+              hover:-translate-y-3 duration-500 ease-in-out 
+              "
               >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current dark:text-white"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
-            </a>
+                <img
+                  src={linkedinIcon}
+                  alt="instagram Icon"
+                  className="h-10 w-10 "
+                />
+              </a>
           </nav>
         </aside>
         <p className="w-full text-center font-bold text-lg md:text-xl dark:text-white">
